99abbf1ab0218446482a44f6014a1d556562c6c9
Ken Kahn <toontalk@gmail.com>
2017-03-12 18:25:29 +0800

    Support 3 vision cloud providers at once
    And rely upon Snap! global variables instead of URL parameters. Fixes #1

--- analyse-camera.js
+++ analyse-camera.js
@@ -1,52 +1,38 @@
-var video = document.createElement('video');
+var video  = document.createElement('video');
 var canvas = document.createElement('canvas');
 
-var get_url_parameter = function (name, prompt_message) {
-    var parts = window.location.search.substr(1).split('&');
+var get_global_variable_value = function (name) {
+    var ancestor = this;
     var value;
-    parts.some(function (part) {
-        var name_and_value = part.split('=');
-        if (name_and_value[0] === name) {
-            value = name_and_value[1];
-            return true;
-        }
-    });
-    if (prompt_message && typeof value === 'undefined') {
-       return window.prompt(prompt_message);
+    while (ancestor && !(ancestor instanceof IDE_Morph)) {
+	ancestor = ancestor.parent;
     }
-    return value;
-};
-
-var update_url = function () {
-    if (!get_url_parameter("key")) {
-       window.alert("The URL is about to be updated to include your key and cloud provider choice. Save this URL so you don't need to provide them each time.");
-       window.onbeforeunload = null; // don't warn about reload
-       // https is needed to access camera etc in some browsers
-       window.location.replace("https://" + window.location.host + window.location.pathname +
-                               "?provider=" + window.cloud_provider + "&key=" + key +
-                               window.location.hash);
+    if (ancestor) {
+	value = ancestor.globalVariables.getVar(name);
+    } else {
+        value =  world.children[0].globalVariables.getVar(name);
     }
-};
+    if (typeof value ===  'string') {
+       return value;
+    }
+    return value.contents;
+}.bind(this);
 
-window.cloud_provider = get_url_parameter("provider", "Please enter either Watson, Google, or Microsoft.");
+var cloud_providers = get_global_variable_value('AI cloud providers');
 
-if (window.cloud_provider !== "Watson" && window.cloud_provider !== "Google" && window.cloud_provider !== "Microsoft") {
-   window.alert("This program will only work if the AI cloud service provider is Watson, Google, or Microsoft. Not " + window.cloud_provider);
-   window.onbeforeunload = null; // don't warn about reload
-   window.location.reload(true);
-}
+var keys = [];
 
-var key = get_url_parameter("key", "Please enter your API key or cancel if you don't have one.");
-
-if (!key) {
-   if (window.confirm("No key provided. Do you want to visit https://github.com/ToonTalk/ai-cloud/wiki to learn how to get a key?")) {
-      window.onbeforeunload = null; // don't warn about reload
-      window.assign("https://github.com/ToonTalk/ai-cloud/wiki");
-   }
-   return;
-}
-
-update_url();
+cloud_providers.forEach(function (provider) {
+    var key = get_global_variable_value(provider + " key");
+    if (key) {
+        keys[provider] = key;
+	return;
+    }
+    if (window.confirm("No key provided for " + provider + ". Do you want to visit https://github.com/ToonTalk/ai-cloud/wiki to learn how to get a key?")) {
+	window.onbeforeunload = null; // don't warn about reload
+	window.assign("https://github.com/ToonTalk/ai-cloud/wiki");
+    }
+});
 
 var startup = function startup() {
     var callback = function(stream) {
@@ -88,7 +74,7 @@ console.log("test this");
 // other changes before drawing it.
 
 // available for external use by attaching it to window
-window.take_picture_and_analyse = function (show_photo, callback) {
+window.take_picture_and_analyse = function (cloud_provider, show_photo, callback) {
     var context, photo;
     if (!callback) {
         callback = console.log;
@@ -116,27 +102,30 @@ window.take_picture_and_analyse = function (show_photo, callback) {
                                            document.body.title = "";
                                        });
     }
-    switch (window.cloud_provider) {
+    switch (cloud_provider) {
     case "Watson":
     case "Microsoft":
-        canvas.toBlob(function (blob) {
-            post_image(blob,
-                       function (event) {
-                           callback(event.currentTarget.response)
-                       });
-        },
-                      "image/png");
+        canvas.toBlob(
+            function (blob) {
+		post_image(blob,
+                           cloud_provider,
+			   function (event) {
+                               callback(event.currentTarget.response);
+			   });
+            },
+            "image/png");
         break;
     case "Google":
         post_image(canvas.toDataURL('image/png'),
+                   cloud_provider,
                    function (event) {
                        callback(event.currentTarget.response);
                    });
         break;
     }
-}
+};
 
-var post_image = function post_image(image, callback, error_callback) {
+var post_image = function post_image(image, cloud_provider, callback, error_callback) {
     // based upon https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/Sending_forms_through_JavaScript
     var XHR = new XMLHttpRequest();
     var formData;
@@ -149,30 +138,31 @@ var post_image = function post_image(image, callback, error_callback) {
         }
     }
     XHR.addEventListener('error', error_callback);
-    switch (window.cloud_provider) {
+    switch (cloud_provider) {
     case  "Watson":
         formData = new FormData();
         formData.append("images_file", image, "blob.png");
-        XHR.open('POST', "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?version=2016-05-19&api_key=" + key);
+        XHR.open('POST', "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?version=2016-05-19&api_key=" + keys.Watson);
         XHR.send(formData);
         break;
     case "Google":
-        XHR.open('POST', "https://vision.googleapis.com/v1/images:annotate?key=" + key);
+        XHR.open('POST', "https://vision.googleapis.com/v1/images:annotate?key=" + keys.Google);
         XHR.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         XHR.send(JSON.stringify({"requests":[{"image":{"content": image.substring("data:image/png;base64,".length)},
                                               "features":[{"type":"LABEL_DETECTION",
-                                                           "maxResults":1},
-                                                          {"type": "TEXT_DETECTION",
-                                                           "maxResults":3},
-                                                          {"type": "FACE_DETECTION",
-                                                           "maxResults":1},
-                                                          {"type": "IMAGE_PROPERTIES",
-                                                           "maxResults":2}]}]
+                                                           "maxResults":32},
+//                                                          {"type": "TEXT_DETECTION",
+//                                                           "maxResults":3},
+//                                                          {"type": "FACE_DETECTION",
+//                                                           "maxResults":1},
+//                                                          {"type": "IMAGE_PROPERTIES",
+//                                                           "maxResults":2}
+]}]
                                 }));
         break;
     case "Microsoft":
         // see https://social.msdn.microsoft.com/Forums/en-US/807ee18d-45e5-410b-a339-c8dcb3bfa25b/testing-project-oxford-ocr-how-to-use-a-local-file-in-base64-for-example?forum=mlapi
-        XHR.open('POST', "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags,Faces,Color,Categories&subscription-key=" + key);
+        XHR.open('POST', "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags,Faces,Color,Categories&subscription-key=" + keys.Microsoft);
         XHR.setRequestHeader('Content-Type', 'application/octet-stream');
         XHR.send(image);
         break;
