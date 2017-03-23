adf4779fecd77b5cb274e45f2320ab4e5b57884b
Ken Kahn <toontalk@gmail.com>
2017-03-23 13:25:02 +0800

    Added speaking features
    pitch, rate, voice, volume, etc.

--- speak-for-snap.js
+++ speak-for-snap.js
@@ -1,6 +1,24 @@
+// see https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
 var utterance = new SpeechSynthesisUtterance(message);
 if (lang) {
    utterance.lang = lang;
+}
+if (pitch > 0) {
+   utterance.pitch = pitch;
+}
+if (rate > 0) {
+   utterance.rate = rate;
+}
+if (voice) {
+   voices = window.speechSynthesis.getVoices();
+   if (voice < voices.length) {
+       utterance.voice = voices[voice];
+   } else {
+       alert("Only " + voices.length + " voices are available so you can't choose voice number " + voice);
+   }
+}
+if (volume > 0) {
+   utterance.volume = volume;
 }
 if (typeof finished_callback === 'object') {
    // callback provided
