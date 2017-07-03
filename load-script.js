a755226359c17abfc576d2bb8993c302aa22fc29
Ken Kahn <toontalk@gmail.com>
2017-07-03 15:34:34 +0100

    List of scripts loaded no longer contains duplicates

--- load-script.js
+++ load-script.js
@@ -11,7 +11,7 @@ script.addEventListener('load', function () {
     if (typeof callback === 'object') {
        invoke(callback, new List([]));
     };
-    if (window.ecraft2learn_scripts_loaded) {
+    if (window.ecraft2learn_scripts_loaded && window.ecraft2learn_scripts_loaded.indexOf(url) < 0) {
         window.ecraft2learn_scripts_loaded.push(url);
     } else {
         window.ecraft2learn_scripts_loaded = [url];
