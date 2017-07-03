685f15ad415046c514804d4dd811cc2633ba4f52
Ken Kahn <toontalk@gmail.com>
2017-07-03 15:19:19 +0100

    Load script now optionally reloads

--- load-script.js
+++ load-script.js
@@ -1,9 +1,20 @@
+if (!reload && window.ecraft2larn_scripts_loaded && window.ecraft2learn_scripts_loaded.indexOf(url) >= 0) {
+    if (typeof callback === 'object') {
+       invoke(callback, new List([]));
+    };
+    return;
+}
 var script = document.createElement("script");
 script.type = "text/javascript";
 script.src = url;
-if (typeof callback === 'object') {
-    script.addEventListener('load', function () {
+script.addEventListener('load', function () {
+    if (typeof callback === 'object') {
        invoke(callback, new List([]));
-    });
-};
+    };
+    if (window.ecraft2learn_scripts_loaded) {
+        window.ecraft2learn_scripts_loaded.push(url);
+    } else {
+        window.ecraft2learn_scripts_loaded = [url];
+    }
+});
 document.head.appendChild(script);
