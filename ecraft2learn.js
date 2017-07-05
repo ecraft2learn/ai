7cb153b18ee83940c7d7268f2beb713bd3944e40
Ken Kahn <toontalk@gmail.com>
2017-07-05 16:58:54 +0100

    Added speech synthesis to library

--- ecraft2learn.js
+++ ecraft2learn.js
@@ -351,5 +351,51 @@ window.ecraft2learn =
 	} else {
 		window.addEventListener('load', startup, false);
 	}
+  },
+  speak: function (message, pitch, rate, voice, volume, finished_callback) {
+  	// see https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
+	var utterance = new SpeechSynthesisUtterance(message);
+	if (language) {
+	   utterance.lang = language;
+	}
+	if (pitch > 0) {
+	   utterance.pitch = pitch;
+	}
+	if (rate > 0) {
+	   if (rate < .1) {
+		  // A very slow rate breaks Chrome's speech synthesiser
+		  rate = .1;
+	   }
+	   if (rate > 2) {
+		  rate = 2; // high rate also breaks Chrome's speech synthesis
+	   }
+	   utterance.rate = rate;
+	}
+	if (voice) {
+	   voices = window.speechSynthesis.getVoices();
+	   if (voice >= 0 && voice < voices.length) {
+		   utterance.voice = voices[Math.floor(voice)];
+	   } else {
+		   alert("Only " + voices.length + " voices are available. You can't choose voice number " + voice);
+	   }
+	}
+	if (volume > 0) {
+	   utterance.volume = volume;
+	}
+	if (typeof finished_callback === 'object') {
+	   // callback provided
+	   utterance.onend = function (event) {
+		   invoke(finished_callback, new List([message]));
+	   };
+	}
+	if (window.speech_recognition) {
+	   window.speech_recognition.abort();
+	}
+	window.speechSynthesis.speak(utterance);
+  },
+  get_voice_names: function () {
+    return new List(window.speechSynthesis.getVoices().map(function (voice) {
+    	return voice.name;
+    }));
   }
   }} ());
