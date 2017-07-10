5eb28e8c4d483ba05cda5d3918b1acf17f26fa8f
Ken Kahn <toontalk@gmail.com>
2017-07-10 18:08:53 +0100

    Use ecraft2learn command responds better to second call
    If before libary script has loaded.

--- use-ecraft2learn-command.js
+++ use-ecraft2learn-command.js
@@ -1,14 +1,18 @@
 "use strict";
+var when_script_loaded = function () {
+	window.ecraft2learn_library_script = undefined;
+	ecraft2learn.snap_context = this;
+        ecraft2learn.run(function_name, parameters);
+    }.bind(this);    
 if (typeof ecraft2learn === 'object') {
     ecraft2learn.snap_context = this;
     ecraft2learn.run(function_name, parameters);
+} else if (window.ecraft2learn_library_script) {
+    window.ecraft2learn_library_script.addEventListener('load', when_script_loaded);
 } else {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://toontalk.github.io/ai-cloud/ecraft2learn.js";
-    script.addEventListener('load', function () {
-	ecraft2learn.snap_context = this;
-        ecraft2learn.run(function_name, parameters);
-    }.bind(this));
+    window.ecraft2learn_library_script = document.createElement("script");
+    window.ecraft2learn_library_script.type = "text/javascript";
+    window.ecraft2learn_library_script.src = "https://toontalk.github.io/ai-cloud/ecraft2learn.js";
+    window.ecraft2learn_library_script.addEventListener('load', when_script_loaded);
     document.head.appendChild(script);
 }
