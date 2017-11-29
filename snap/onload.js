96a09f76e74970fc43eaebe39451f18ec44619cc
Ken Kahn <toontalk@gmail.com>
2017-11-29 12:04:41 +0100

    onload handler works outside of an iframe
    Also made loop local

--- snap/onload.js
+++ snap/onload.js
@@ -1,23 +1,26 @@
 var world;
-			window.onload = function () {
-				var world_canvas = document.getElementById('world');
-				var ide_morph = new IDE_Morph();
-				world = new WorldMorph(world_canvas);
-//                 world.worldCanvas.focus();
-				ide_morph.openIn(world);
-				var project_path = window.frameElement.getAttribute("project_path");
-				if (project_path) {
-					fetch(project_path).then(function (response) {
-												 response.text().then(function (project_text) {
-											 		 ide_morph.rawOpenProjectString(project_text);
-											 	 });
-											 }).catch(function (error) {
-												 console.error("Error fetching " + project_path + ": " + error.message);
-											 });
-				}
-                loop();
-			};
-			function loop() {
-                requestAnimationFrame(loop);
-				world.doOneCycle();
-			}
+window.onload = function () {
+	var world_canvas = document.getElementById('world');
+	var ide_morph = new IDE_Morph();
+	var loop = function loop() {
+		requestAnimationFrame(loop);
+		world.doOneCycle();
+	};
+	world = new WorldMorph(world_canvas);
+//  world.worldCanvas.focus();
+	ide_morph.openIn(world);
+	if (window.frameElement) { // if running in an iframe see if a local project_path is declared
+		var project_path = window.frameElement.getAttribute("project_path");
+		if (project_path) {
+			fetch(project_path).then(function (response) {
+										 response.text().then(function (project_text) {
+											 ide_morph.rawOpenProjectString(project_text);
+										 });
+									 }).catch(function (error) {
+										 console.error("Error fetching " + project_path + ": " + error.message);
+									 });
+		}
+	}
+    loop();
+};
+
