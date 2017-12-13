eed304b3b03ae2f56785575836eee9ecdb637139
Ken Kahn <toontalk@gmail.com>
2017-12-13 14:04:17 +0100

    All chapters share the same acknowledgement section

---
+++ AI-Teacher-Guide/acknowledgements.js
@@ -0,0 +1,8 @@
+document.write(
+'<h3>Acknowledgements</h3>' +
+'<p>This research was supported by the <a href="https://project.ecraft2learn.eu/" target="_blank">eCraft2Learn project</a> ' +
+"funded by the European Union's Horizon 2020 Coordination & Research and Innovation Action " +
+'under Grant Agreement No 731345. ' +
+'<img src="https://project.ecraft2learn.eu/wp-content/uploads/2017/01/eCraft2Learn-Final0.1.png" alt="eCraft2Learn icon"> ' +
+'</p>');
+document.close();
