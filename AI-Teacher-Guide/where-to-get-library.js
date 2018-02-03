ef2a9f362792a0f44f4b3ae903b4bf614242aabc
Ken Kahn <toontalk@gmail.com>
2018-02-03 15:27:28 +0700

    Updated link to eCraft2Learn library project

--- AI-Teacher-Guide/where-to-get-library.js
+++ AI-Teacher-Guide/where-to-get-library.js
@@ -4,6 +4,6 @@ document.write(
 '<a href="https://snap.berkeley.edu/" target="_blank">Snap!</a> in ' +
 'this <a href="https://snap.berkeley.edu/snapsource/snap.html#present:Username=toontalk&ProjectName=eCraft2Learn" target="_blank">Snap project</a>. ' +
 'They can also be used together with blocks for controlling Arduinos by downloading and then importing ' +
-'<a href="https://toontalk.github.io/ai-cloud/AI-Teacher-Guide/eCraft2Learn%20S4A.xml" download>this file</a> ' +
+'<a href="https://ecraft2learn.github.io/ai/AI-Teacher-Guide/eCraft2Learn%20S4A.xml" download>this file</a> ' +
 'into <a href="http://snap4arduino.rocks/" target="_blank">Snap4Arduino</a>.');
 document.close();
