9f1b11ee3408a2afd2a728b9f303517b1350fd0d
Ken Kahn <toontalk@gmail.com>
2018-02-28 14:09:37 +0700

    Call eCraft2Learn reporter optionally ignores library not loaded

--- run-ecraft2learn-command-new.js
+++ run-ecraft2learn-command-new.js
@@ -4,7 +4,7 @@ if (typeof ecraft2learn === 'object') {
    var script = document.createElement("script");
    script.type = "text/javascript";
    script.addEventListener('error', function () {
-          alert("Unable to load https://ecraft2learn.github.io/ai/ecraft2learn.js. Check your network connection.";
+          alert("Unable to load https://ecraft2learn.github.io/ai/ecraft2learn.js. Check your network connection.");
    });
    script.src = "https://ecraft2learn.github.io/ai/ecraft2learn.js";
    script.addEventListener('load', function () {
