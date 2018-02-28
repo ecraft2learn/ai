cdb8668cb8575a0d93621230246eeb5ca8d94723
Ken Kahn <toontalk@gmail.com>
2018-02-28 13:27:24 +0700

    Added error handling to run_command

--- run-ecraft2learn-command-new.js
+++ run-ecraft2learn-command-new.js
@@ -3,6 +3,9 @@ if (typeof ecraft2learn === 'object') {
 } else {
    var script = document.createElement("script");
    script.type = "text/javascript";
+   script.addEventListener('error', function () {
+          alert("Unable to load https://ecraft2learn.github.io/ai/ecraft2learn.js. Check your network connection.";
+   });
    script.src = "https://ecraft2learn.github.io/ai/ecraft2learn.js";
    script.addEventListener('load', function () {
           ecraft2learn.run(function_name, parameters);
