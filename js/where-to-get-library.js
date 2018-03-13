629d7c2321b5696902e5f08c11257ca39d359d6f
Ken Kahn <toontalk@gmail.com>
2018-03-13 15:28:20 +0700

    Updated description of how to get the AI blocks.

--- js/where-to-get-library.js
+++ js/where-to-get-library.js
@@ -1,9 +1,9 @@
 document.write(
 '<h3 id="library">Where to get these blocks to use in projects</h3> ' +
-'<p>All of the blocks described in these three chapters are available to use in ' +
+'<p>All of the blocks described in these chapters are available to use in ' +
 '<a href="https://snap.berkeley.edu/" target="_blank">Snap!</a> in ' +
 'this <a href="https://snap.berkeley.edu/snapsource/snap.html#present:Username=toontalk&ProjectName=eCraft2Learn&editMode" target="_blank">Snap project</a>. ' +
 'They can also be used together with blocks for controlling Arduinos by downloading and then importing ' +
 '<a href="https://ecraft2learn.github.io/ai/AI-Teacher-Guide/eCraft2Learn%20S4A.xml" download>this file</a> ' +
-'into <a href="http://snap4arduino.rocks/" target="_blank">Snap4Arduino</a>.');
+'into <a href="/uui/snap4arduino/" target="_blank">Snap4Arduino</a>.');
 document.close();
