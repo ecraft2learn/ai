f618a7bb446fe352007aeb98ecef0a7382025b93
Ken Kahn <toontalk@gmail.com>
2018-03-13 15:14:38 +0700

    Updated creative commons and icons

--- js/bottom-of-page.js
+++ js/bottom-of-page.js
@@ -1,4 +1,10 @@
 document.write(
-'<p>&nbsp;</p>' +
-'<p><img src="/ai/images/Logo_EU_white.png" alt="" width="330" height="66" />&nbsp; &nbsp;<img src="/ai/images/Logo_PD.png?raw=true" alt="" width="60" height="60" />&nbsp; &nbsp;</p>');
-document.close();
+'<p><a href="http://creativecommons.org/licenses/by/4.0/" rel="license"><img style="border-width: 0;" src="/ai/images/88x31.png" alt="Creative Commons License by 4.0"></a>' +
+'&nbsp;&nbsp;' +
+'<img src="/ai/images/logo_EU_flag.png" alt="" width="45" height="30">' +
+'&nbsp;' +
+'<a href="https://project.ecraft2learn.eu"><img src="/ai/images/logo_ecraft_face.png" alt="" width="35" height="30"></a>' +
+'&nbsp;' +
+'eCraft2Learn H2020-731345 - University of Oxford.');
+
+
