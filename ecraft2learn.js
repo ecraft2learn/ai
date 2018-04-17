f6fe4cf16208920d147c28b435848c43042ae5d2
Ken Kahn <toontalk@gmail.com>
2018-04-17 12:19:23 +0100

    Audio training iframe creation checks no CORS problem

--- ecraft2learn.js
+++ ecraft2learn.js
@@ -463,8 +463,18 @@ window.ecraft2learn =
                                           training_window.close();
                                       });
           } else {
+              if (window.location.href.indexOf("https://ecraft2learn.github.io/") !== 0) {
+                  inform("Unable to open audio training frame",
+                         "Due to browser security concerns the audio training frame and Snap! need to come from the same domain.\n" +
+                         "Audio training will work if you run Snap! from https://ecraft2learn.github.io/ai/snap/.\n" +
+                         "Do you want to open this version of Snap! now? (Answer NO if you haven't saved your work.)",
+                         function() {
+                             window.location.replace("https://ecraft2learn.github.io/ai/snap/");
+                         });
+                  return;
+              }
               URL = "https://ecraft2learn.github.io/ai/microphone-train/index.html?translate=1";
-              var iframe = document.createElement('iframe');
+              let iframe = document.createElement('iframe');
               document.body.appendChild(iframe);
               iframe.src = URL;
               iframe.style.width  = '100%';
