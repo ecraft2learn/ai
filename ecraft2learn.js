c243748357b76c09e0393cf057cde806e473e226
Ken Kahn <toontalk@gmail.com>
2018-07-13 10:31:45 +0100

    Better error handling while loading scripts and word embeddings

--- ecraft2learn.js
+++ ecraft2learn.js
@@ -8,7 +8,7 @@
 window.ecraft2learn =
   (function () {
       let this_url = document.querySelector('script[src*="ecraft2learn.js"]').src; // the URL where this library lives
-      let load_script = function (url, when_loaded) {
+      let load_script = function (url, when_loaded, if_error) {
           var script = document.createElement("script");
           script.type = "text/javascript";
           if (url.indexOf("//") < 0) {
@@ -19,6 +19,9 @@ window.ecraft2learn =
           script.src = url;
           if (when_loaded) {
               script.addEventListener('load', when_loaded);
+          }
+          if (if_error) {
+              script.addEventListener('error', if_error);
           }
           document.head.appendChild(script);
       };
@@ -755,7 +758,8 @@ window.ecraft2learn =
         });
         return matching_language_entry; // could be undefined
     };
-    const extract_language_code = function (language) {
+    const extract_language_code = function (language_string) {
+        let language = language_string;
         if (language) {
             if (language.length === 2) {
                 return language;
@@ -763,6 +767,10 @@ window.ecraft2learn =
             language = language_entry(language);
             if (language) {
                 return language[1].substring(0, 2);
+            } else {
+                inform("Unknown language",
+                       "Unable to understand what language '" + language_string + "' is.\n" + 
+                       "Using the current default language: " + (ecraft2learn.default_language_name || "English"));
             }
         }
         if (ecraft2learn.default_language) {
@@ -1189,16 +1197,16 @@ window.ecraft2learn =
         } else if (ecraft2learn.default_language !== matching_language_entry[1]) {
             // default has been changed so notify user
             ecraft2learn.default_language = matching_language_entry[1];
-            var matching_language_name = matching_language_entry[2];
+            ecraft2learn.default_language_name = matching_language_entry[2];
             var mary_tts_voice_number = mary_tts_voice_number_with_language_code(matching_language_entry[1]);
-            var message = "Speech recognition will expect " + matching_language_name + " to be spoken.\n";
+            var message = "Speech recognition will expect " + ecraft2learn.default_language_name + " to be spoken.\n";
             var no_voices_callback = function () {
                 if (mary_tts_voice_number >= 0) {
                     message += "No matching browser speech synthesis voice found but Mary TTS voice " +
                                mary_tts_voices[mary_tts_voice_number-1][1] + " can be used.\n" +
                                "Use the Speak (using Mary TTS engine) command.";
                 } else {
-                    message += "No speech synthesis support for " + matching_language_name + " found so English will be used.";
+                    message += "No speech synthesis support for " + ecraft2learn.default_language_name + " found so English will be used.";
                 }
                 inform("Default language set", message);
             };
@@ -1850,26 +1858,27 @@ window.ecraft2learn =
   show_message: show_message,
   // some word embedding functionality
   dot_product: dot_product,
-  word_embeddings_ready: function (language, word_embeddings_url) {
+  word_embeddings_ready: function (language, callback, word_embeddings_url) {
       language = extract_language_code(language);
       if (typeof words_to_features[language] === 'object') {
-          return true;
+          invoke_callback(callback, "loaded");
+          return;
       }
       if (typeof words_to_features[language] !== 'object') {
-          if (!loading_word_embeddings) {
-              loading_word_embeddings = true;
-              if (!word_embeddings_url) {
-                  word_embeddings_url = "word-embeddings/" + language + "/wiki-words.js";
-              }
-              show_message("Loading script...");
-              load_script(word_embeddings_url,
-                          function () {
-                              loading_word_embeddings = false;
-                              show_message("");
-                          });
+          if (!word_embeddings_url) {
+              word_embeddings_url = "word-embeddings/" + language + "/wiki-words.js";
           }
+          show_message("Loading words ...");
+          load_script(word_embeddings_url,
+                      function () {
+                          show_message("");
+                          invoke_callback(callback, "loaded");
+                      },
+                      function (event) {
+                          show_message("Error while loading '" + word_embeddings_url + "'.\nTry another language.");
+                          invoke_callback(callback, "error");
+                      });
       }
-      return false;
   },
   word_to_features: function (word, language) {
       language = extract_language_code(language);
