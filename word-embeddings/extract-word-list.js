48bff620f5b17924daa9ae13ebc03e838eedfdd2
Ken Kahn <toontalk@gmail.com>
2018-08-16 17:41:11 +0100

    Updated word embedding scripts

--- word-embeddings/extract-word-list.js
+++ word-embeddings/extract-word-list.js
@@ -2,7 +2,8 @@ function extract () {
     // extracts a word list from pages such as http://1000mostcommonwords.com/1000-most-common-italian-words/
     let result = "";
     let tds = document.getElementsByTagName('td');
-    var letters = /^[a-z]+$/; // only lower case letters
+    const letters = /^[a-z]+$/; // only lower case letters to avoid proper nouns
+    // but for Chinese commented out the match and used toLowerCase()
     for (let i = 4; i < 3003; i += 3) {
         if (tds.item(i+1).innerText.match(letters)) {
             result += tds.item(i).innerText + " " + tds.item(i+1).innerText + "\n";
