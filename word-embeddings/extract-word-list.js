7b1bfcf63cabd90edc0d2722a7bbdc78889a86b1
Ken Kahn <toontalk@gmail.com>
2018-08-16 21:06:52 +0100

    Translated Hindi word embeddings

--- word-embeddings/extract-word-list.js
+++ word-embeddings/extract-word-list.js
@@ -6,9 +6,9 @@ function extract () {
     // but for Chinese commented out the match and used toLowerCase()
     for (let i = 4; i < 3003; i += 3) {
         if (tds.item(i+1).innerText.match(letters)) {
-            result += tds.item(i).innerText + " " + tds.item(i+1).innerText + "\n";
-        }
-        
+            // Hindi file has spaces in words
+            result += tds.item(i).innerText.split(' ').join('') + " " + tds.item(i+1).innerText + "\n";
+        }    
     }
     let new_window = window.open("");
     new_window.document.body.innerText = result;
