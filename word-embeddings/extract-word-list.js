0c519827ea2d44ac8848d306d4d74a4a6c0488a8
Ken Kahn <toontalk@gmail.com>
2018-08-18 17:57:40 +0100

    Updated word list extraction and some examples

--- word-embeddings/extract-word-list.js
+++ word-embeddings/extract-word-list.js
@@ -4,10 +4,18 @@ function extract () {
     let tds = document.getElementsByTagName('td');
     const letters = /^[a-z]+$/; // only lower case letters to avoid proper nouns
     // but for Chinese commented out the match and used toLowerCase()
-    for (let i = 4; i < 3003; i += 3) {
-        if (tds.item(i+1).innerText.match(letters)) {
-            // Hindi file has spaces in words
-            result += tds.item(i).innerText.split(' ').join('') + " " + tds.item(i+1).innerText + "\n";
+    let output_count = 0;
+    for (let i = 4; i < 3003; i += 3) { // 3003 for all 1000 (except those that fail the following tests)
+        let english_word = tds.item(i+1).innerText;
+        let other_word = tds.item(i).innerText.toLowerCase();
+        if (english_word.match(letters) &&
+            english_word.indexOf(" ") < 0 && // single word
+            other_word.indexOf(" ") < 0) {
+            result += other_word + " " + english_word + "\n";
+            output_count++;
+            if (output_count === 500) {
+                break;
+            }
         }    
     }
     let new_window = window.open("");
