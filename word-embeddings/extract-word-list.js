5d1c2c4f331f45fc30a865639acfd5d7dc8d934f
Ken Kahn <toontalk@gmail.com>
2018-08-28 15:06:18 +0100

    Fixed the German word embeddings

--- word-embeddings/extract-word-list.js
+++ word-embeddings/extract-word-list.js
@@ -1,3 +1,6 @@
+// after opening a language page at http://1000mostcommonwords.com
+// copy and paste this into the console for the page and then run extract()
+
 function extract () {
     // extracts a word list from pages such as http://1000mostcommonwords.com/1000-most-common-italian-words/
     let result = "";
@@ -5,9 +8,11 @@ function extract () {
     const letters = /^[a-z]+$/; // only lower case letters to avoid proper nouns
     // but for Chinese commented out the match and used toLowerCase()
     let output_count = 0;
-    for (let i = 4; i < 3003; i += 3) { // 3003 for all 1000 (except those that fail the following tests)
+    for (let i = 4; i < 3003; i += 3) { 
+        // 3003 for all 1000 (except those that fail the following tests)
+        // but this stops after finding 500 entries
         let english_word = tds.item(i+1).innerText;
-        let other_word = tds.item(i).innerText.toLowerCase();
+        let other_word = tds.item(i).innerText.toLowerCase(); 
         if (english_word.match(letters) &&
             english_word.indexOf(" ") < 0 && // single word
             other_word.indexOf(" ") < 0) {
