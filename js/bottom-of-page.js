6d72fefb9e0511e76aa098e0526fd3333b5ffa88
Ken Kahn <toontalk@gmail.com>
2018-11-17 11:06:49 +0000

    Made logos on bottom of pages bigger

--- js/bottom-of-page.js
+++ js/bottom-of-page.js
@@ -5,12 +5,12 @@ document.write(
      '<p class="guide-to-guide">Go to <a href="/ai/">the AI home page</a></p>') +
 '<p><a href="http://creativecommons.org/licenses/by/4.0/" rel="license"><img style="border-width: 0;" src="/ai/images/88x31.png" alt="Creative Commons License by 4.0"></a>' +
 '&nbsp;&nbsp;' +
-'<img src="/ai/images/logo_EU_flag.png" alt="" width="45" height="30">' +
+'<img src="/ai/images/logo_EU_flag.png" alt="" width="90" height="60">' +
 '&nbsp;' +
-'<a href="https://project.ecraft2learn.eu"><img src="/ai/images/logo_ecraft_face.png" alt="" width="35" height="30"></a>' +
+'<a href="https://project.ecraft2learn.eu"><img src="/ai/images/logo_ecraft_face.png" alt="" width="70" height="60"></a>' +
 '&nbsp;' +
 'eCraft2Learn H2020-731345 -&nbsp;' +
-'<img src="/ai/images/oxford-crestsq.png" alt="" width="45" height="45">' +
+'<img src="/ai/images/oxford-crestsq.png" alt="" width="60" height="60">' +
 '&nbsp;' +
 'University of Oxford</p>');
 
