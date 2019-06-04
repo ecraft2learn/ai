216001789dc66f5f8ca961dda4e4fe295c4483c2
Ken Kahn <toontalk@gmail.com>
2019-06-04 17:27:13 +0100

    Added two clases to Korean images

--- onyx/korean.js
+++ onyx/korean.js
@@ -1,10 +1,24 @@
 const images = {
     "normal": [{file_name: "images/korean/L_NAIL_5nail_5nail2010_1_normalnail.png",
-               count: 150,},
-               {file_name: "images/korean/L_NAIL_5nail_5nail2014_1_normalnail.png",
-                count: 150}],
-    "fungal": [{file_name: "images/korean/L_NAIL_5nail_5nail2004_2_onychomycosis.png",
-               count: 150,},
+                count: 250,
+                dimensions: {width: 2260,
+                             height: 7876}},
+//                {file_name: "images/korean/L_NAIL_5nail_5nail2014_1_normalnail.png",
+//                 dimensions: {width: 2260,
+//                              height: 9156}}
+              ],
+    "fungal": [
+//                {file_name: "images/korean/L_NAIL_5nail_5nail2004_2_onychomycosis.png",
+//                 dimensions: {width: 2260,
+//                              height: 1092}},
                {file_name: "images/korean/L_NAIL_5nail_5nail2006_onychomycosis_onychomycosis.png",
-               count: 150,}],
+                dimensions: {width: 2260,
+                             height: 1732}}
+              ],
+    "melanonychia": [{file_name: "images/korean/L_NAIL_5nailA2_lesion_melanonychia3_melanonychia.png",
+                      dimensions: {width: 2260,
+                                   height: 1860}}],
+    "onycholysis": [{file_name: "images/korean/L_NAIL_5nailA2_lesion_naildystrophy_onycholysis.png",
+                     dimensions: {width: 2260,
+                                  height: 1860}}],
 };
