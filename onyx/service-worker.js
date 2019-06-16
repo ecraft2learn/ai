d83adb195a112086f390bba69f4dec3b88d670b8
Ken Kahn <toontalk@gmail.com>
2019-06-16 06:40:50 +0100

    Fixed cache name for PWA

--- onyx/service-worker.js
+++ onyx/service-worker.js
@@ -48,7 +48,7 @@ self.addEventListener('fetch', function(e) {
     caches.match(e.request).then(function(r) {
           console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
+                return caches.open(cache_name).then(function(cache) {
           console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
