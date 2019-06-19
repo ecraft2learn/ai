86d41a39efbfa3c1d1c1aa3dc4072312853aa03e
Ken Kahn <toontalk@gmail.com>
2019-06-19 10:33:43 +0100

    Created trauma test file

---
+++ onyx/trauma.js
@@ -0,0 +1,21 @@
+add_to_dataset(
+{"normal": [],
+"non-serious": [
+"images/trauma/nail trauma 1.png",
+"images/trauma/nail trauma 3.png",
+"images/trauma/nail trauma 4.png",
+"images/trauma/nail trauma 5.png",
+"images/trauma/nail trauma 6.png",
+"images/trauma/nail trauma 7.png",
+"images/trauma/nail trauma 8.png",
+"images/trauma/nail trauma 9.png",
+"images/trauma/nail trauma 10.png",
+"images/trauma/nail trauma 11.png",
+"images/trauma/nail trauma 12.png",
+"images/Nail Images from Library book/minor trauma nail 1 habit tic deformity-a.jpg",
+"images/Nail Images from Library book/minor trauma nail 1 habit tic deformity-b.jpg",
+"images/Nail Images from Library book/minor trauma nail 2 habit tic deformity.jpg",
+"images/trauma/Karen IMG_1621-a.jpg",
+],
+"warrants second opinion": []
+});
