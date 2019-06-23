50229a18344249a496ce3013124c28314b1f923f
Ken Kahn <toontalk@gmail.com>
2019-06-23 14:41:24 +0100

    Logging enabled if log=<user id> is added to URL

---
+++ snap/snap_or_isnap.js
@@ -0,0 +1,13 @@
+// either launches the Snap! release or iSnap which supports logging
+
+const hash_parameters = new URLSearchParams(window.location.hash.slice(1));
+const search_parameters = new URLSearchParams(window.location.search);
+const user_id_for_logs = search_parameters.get('log') || hash_parameters.get('log');
+const source =  user_id_for_logs ?
+               "../isnap/snap-logging.html" :
+               "snap-no-logging.html";
+let search = window.location.search;
+if (user_id_for_logs) {
+	search += "&user=" + user_id_for_logs;
+}
+document.location.replace(source + search + window.location.hash);
