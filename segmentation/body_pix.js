2cce801b290ea280bbc9fe49b855098b159f92c4
Ken Kahn <toontalk@gmail.com>
2020-05-10 15:34:29 +0100

    Saving a local version of Body PIX for segmentation

--- segmentation/body_pix.js
+++ segmentation/body_pix.js
@@ -23,20 +23,41 @@ const respond_to_messages =
             const image_url = event.data.segmentations_and_poses.image_url;
             const options = event.data.segmentations_and_poses.options;
             const config = options && options.config;
-            // timestamp?? 
             const when_image_loaded =
                 async (image) => {
                     console.time("segmentation");
                     const segmentations = await segmenter.segmentMultiPersonParts(image, config);
                     console.timeLog("segmentation");
-                    const color_mappings = options["color mappings"];
+                    let color_mappings = options["color mappings"];
+                    let color_mapping;
                     if (color_mappings) {
-                        segmentations.forEach((segmentation, index) => {
-                            let color_mapping = color_mappings.length > 1 ? color_mappings[index] : color_mappings[0];
+                        color_mappings = color_mappings.map((color_mapping) => {
                             if (typeof color_mapping[0][0] === 'string') {
                                 // remove body part names
-                                color_mapping = color_mapping.map((mapping) => mapping[1]);
+                                return color_mapping.map((mapping) => mapping[1]);
+                            } else {
+                                return color_mapping;
                             }
+                        });
+                    }
+                    if (options["create costume"]) {
+                        if (color_mappings && color_mappings.length > 1) {
+                            // each segmentation (i.e. person) has a different color mapping
+                            segmentations.forEach((segmentation, index) => {
+                                segmentations[index].mask = bodyPix.toColoredPartMask(segmentations, color_mappings[index].slice(1));
+                            });
+                        } else {
+                            const color_mapping = color_mappings && color_mappings[0].slice(1); // .map((rgba) => rgba.slice(0, 3));
+                            // if no color_mappings then default rainbow is used
+                            const mask = bodyPix.toColoredPartMask(segmentations, color_mapping);
+                            segmentations.forEach((segmentation) => {
+                                segmentation.mask = mask; // they all share the same mask
+                            });
+                        }
+                        console.timeLog("segmentation");
+                    } else if (color_mappings) {
+                        segmentations.forEach((segmentation, index) => {
+                            color_mapping = color_mappings.length > 1 ? color_mappings[index] : color_mappings[0];
                             segmentation.pixels = [];
                             segmentation.data.forEach((part_id) => {
                                 segmentation.pixels.push(color_mapping[part_id+1]); // +1 since -1 is "no body part"
