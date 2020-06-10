175b450e5ab27aca27b784ead64955b9a7f766a2
Ken Kahn <toontalk@gmail.com>
2020-06-10 19:12:07 +0100

    Added object detection sample project to home page

--- js/ai-guide.js
+++ js/ai-guide.js
@@ -212,7 +212,6 @@ window.addEventListener(
          }
          Array.prototype.forEach.call(elements, add_search_and_hash);
          Array.prototype.forEach.call(document.getElementsByTagName('a'), add_search_and_hash);
-         
 });
 
 // hide the next sibling of advanced-topic elements until clicked
