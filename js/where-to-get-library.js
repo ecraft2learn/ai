5caabf8d497514a166461aeef0d282adf6657cd5
Ken Kahn <toontalk@gmail.com>
2021-06-22 16:45:49 +0100

    Replaced obsolete end of each chapter with current library and project links

--- js/where-to-get-library.js
+++
@@ -1,9 +0,0 @@
-document.write(
-'<h4 id="library">Where to get these blocks to use in projects</h4> ' +
-'<p class="guide-to-guide">All of the blocks described in these chapters are available to use in ' +
-'<a href="https://snap.berkeley.edu/" target="_blank">Snap!</a> in ' +
-'this <a href="https://snap.berkeley.edu/snapsource/snap.html#present:Username=toontalk&ProjectName=eCraft2Learn&editMode" target="_blank">Snap project</a>. ' +
-'They can also be used together with blocks for controlling Arduinos by downloading and then importing ' +
-'<a href="https://ecraft2learn.github.io/ai/AI-Teacher-Guide/eCraft2Learn%20S4A.xml" download>this file</a> ' +
-'into <a href="https://ecraft2learn.github.io/uui/snap4arduino/" target="_blank">Snap4Arduino</a>.</p>');
-document.close();
