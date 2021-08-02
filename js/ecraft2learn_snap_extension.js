3813f20d14b64864163d431f47ddd69d80b1c3e7
Ken Kahn <toontalk@gmail.com>
2021-08-02 15:15:45 +0100

    Simplified Snap! extension and added logging

--- js/ecraft2learn_snap_extension.js
+++ js/ecraft2learn_snap_extension.js
@@ -1,13 +1,14 @@
 SnapExtensions.primitives.set(
     'e2l_run(command_name, parameters)',
     (command_name, parameters, process) => {
-        ecraft2learn.run(command_name, parameters, this.parentThatIsA(IDE_Morph), this.parentThatIsA(StageMorph), process);
+        console.log(this);
+        ecraft2learn.run(command_name, parameters); //, this.parentThatIsA(IDE_Morph), this.parentThatIsA(StageMorph), process);
     }
 );
 
 SnapExtensions.primitives.set(
     'e2l_call(reporter_name, parameters)',
     (reporter_name, parameters, process) => {
-        return ecraft2learn.run(reporter_name, parameters, this.parentThatIsA(IDE_Morph), this.parentThatIsA(StageMorph), process);
+        return ecraft2learn.run(reporter_name, parameters); //, this.parentThatIsA(IDE_Morph), this.parentThatIsA(StageMorph), process);
     }
 );
