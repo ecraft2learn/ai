d4d6b9260335e963b40c949f24ba070a3a10c053
Ken Kahn <toontalk@gmail.com>
2022-01-05 16:42:00 +0000

    Fixed deletion of old versions of a service worker

--- js/service-worker-utilities.js
+++ js/service-worker-utilities.js
@@ -22,10 +22,9 @@ function active_listener (event, cache_name) {
             console.log("Service worker " + cache_name + " activating");
             return Promise.all(keyList.map((key) => {
                 if (key !== cache_name) {
-                    const last_hypen = key.lastIndexOf('-');
                     console.log(key + " " + cache_name + " " + 
-                                key.substring(0, last_hypen) + " " + cache_name.substring(0, last_hypen));
-                    if (key.substring(0, last_hypen) === cache_name.substring(0, last_hypen)) {
+                                key.substring(0, key.lastIndexOf('-')) + " " + cache_name.substring(0, cache_name.lastIndexOf('-')));
+                    if (key.substring(0, key.lastIndexOf('-')) === cache_name.substring(0, cache_name.lastIndexOf('-'))) {
                         // only delete old if different version of the same cache 
                         console.log("Deleting " + key + " since not equal to " + cache_name);
                         return caches.delete(key);
