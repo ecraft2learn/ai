175ed6ef71b6aba3d47e112db7fded3270a656e0
Ken Kahn <toontalk@gmail.com>
2022-01-05 14:52:40 +0000

    Increment version of PWA for new manifest

--- snap/sw.js
+++ snap/sw.js
@@ -2,7 +2,7 @@ importScripts('/ai/js/service-worker-utilities.js');
 
 var snapVersion = '7.0.1'
 
-const cache_name = 'snap-ai-v7';
+const cache_name = 'snap-ai-v8';
 
 const files_to_cache = [
         '/ai/snap/snap.html',
