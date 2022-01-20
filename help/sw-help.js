e76a9bda4dd03c95a9b03b7abdf410a8b6bac31e
Ken Kahn <toontalk@gmail.com>
2022-01-20 09:22:50 +0000

    Updated help pages about running offline

--- help/sw-help.js
+++ help/sw-help.js
@@ -1,14 +1,16 @@
 importScripts('/ai/js/service-worker-utilities.js');
 
-const cache_name = 'snap-help-v2',
+const cache_name = 'snap-help-v3',
     files_to_cache = [
 'hyperparameter-search.html', // from Snap! help block
 'AI QnA explanation.html',
 'hyperparameters_search.html', // from TensorFlow support iframe
 'layers_help.html',
 'training_help.html',
+'progressive-web-app.html',
 'noisy-polygons-project-ideas.html',
-'images/hyperparameter-search.png'
+'images/hyperparameter-search.png',
+'images/install-pwa.png'
 
 ];
 
