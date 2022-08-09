15f35c326678d7a8ec5728442193a2a1b15871db
Ken Kahn <toontalk@gmail.com>
2022-08-09 15:49:14 +0100

    Improved gathering the plain text from the Snap! manual

--- js/manual-gather.js
+++ js/manual-gather.js
@@ -1,6 +1,12 @@
 window.page = 3;
 window.manual = "";
 
+// .s2_144 {
+//     font-size: 18px;
+//     font-family: Baskerville-Bold_1xs;
+//     color: #000;
+// }
+
 window.addEventListener('DOMContentLoaded', (event) => {
     const last_page = 158; // skip the index
     const iframe = document.createElement('iframe');
@@ -10,7 +16,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
     iframe.addEventListener('load', (event) => {
         const text_containers = iframe.contentDocument.body.getElementsByClassName('text-container');
         if (text_containers.length > 0) { // first one is empty - not sure why
-            window.manual += text_containers[0].innerText.replace(/\s+/g, ' ');  // thanks Codex
+            const section_headers = iframe.contentDocument.getElementsByClassName('s2_144');
+            for (let i = 0; i < section_headers.length; i++) {
+                // so headers aren't joined to the first following sentence
+                section_headers[i].innerText = section_headers[i].innerText.trim() + '.'; 
+            }
+            let page_text = text_containers[0].innerText.replace(/\s+/g, ' ') // thanks Codex
+                              .replaceAll(' ,', ',') // happens frequently in the manual due to image of black
+                              .replaceAll('“ ', '“') // and quoted text gets extra spaces
+                              .replaceAll(' ”', '”')
+            const first_space = page_text.indexOf(' ');
+            page_text = page_text.substring(first_space+1); // remove page number
+            window.manual += page_text;
             console.log("Loaded " + iframe.src + " manual length is " + manual.length);
         }
         if (window.page < last_page) {
